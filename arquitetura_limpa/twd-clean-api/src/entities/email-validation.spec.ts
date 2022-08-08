693a470b2853d5ae8cf8c02fac2732d0d149987b
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-08 20:50:32 -0300

    test: validate emails with domain larger than 255 chars

--- arquitetura_limpa/twd-clean-api/src/entities/email-validation.spec.ts
+++ arquitetura_limpa/twd-clean-api/src/entities/email-validation.spec.ts
@@ -17,13 +17,18 @@ describe("E-mail validation", () => {
     expect(Email.validate(email)).toBeTruthy();
   });
 
-  test("should not accept local part large tan 64 chars", () => {
+  test("should not accept local part large than 64 chars", () => {
     const email = 'l'.repeat(65) + "@email.com";
     expect(Email.validate(email)).toBeFalsy();
   });
 
   test("should not accept strings large than 320 chars", () => {
     const email = 'c'.repeat(64) + "@" + 'd'.repeat(127) + "." + 'c'.repeat(128);
+    expect(Email.validate(email)).toBeFalsy();
+  });
+
+  test("should not accept domain part large than 255 chars", () => {
+    const email = "local@" + 'd'.repeat(128) + '.' + 'd'.repeat(127);
     expect(Email.validate(email)).toBeFalsy();
   })
 
