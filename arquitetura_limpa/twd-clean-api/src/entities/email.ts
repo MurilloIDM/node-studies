7a0c6568e758235fc3f96f476d761bf0f9686f3f
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-08 20:37:54 -0300

    feat: validate local part larger 64 chars

--- arquitetura_limpa/twd-clean-api/src/entities/email.ts
+++ arquitetura_limpa/twd-clean-api/src/entities/email.ts
@@ -1,8 +1,10 @@
 export class Email {
   static validate(email: string): boolean {
-    if (!email) {
-      return false;
-    }
+    if (!email) return false;
+
+    const [local, domain] = email.split("@");
+
+    if (local.length > 64) return false;
 
     return true;
   }
