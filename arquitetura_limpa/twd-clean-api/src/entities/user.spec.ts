1ad5169044a0cfdb0094d7f7cbceee5d3dd9396e
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-15 20:29:36 -0300

    feat: verify name while creating user

--- arquitetura_limpa/twd-clean-api/src/entities/user.spec.ts
+++ arquitetura_limpa/twd-clean-api/src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from "../shared/either";
 import { InvalidEmailError } from "./errors/invalid-email-error";
+import { InvalidNameError } from "./errors/invalid-name-error";
 import { User } from "./user";
 
 describe("User domain entity", () => {
@@ -10,5 +11,19 @@ describe("User domain entity", () => {
 
     expect(error).toEqual(left(new InvalidEmailError()));
   });
+
+  test("should not create user with invalid name (too few characters)", () => {
+    const invalidName = "O      ";
+    const error = User.create({ name: invalidName, email: "any@email.com" });
+
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  test("should not create user with invalid name (too many characters)", () => {
+    const invalidName = 'O'.repeat(257);
+    const error = User.create({ name: invalidName, email: "any@email.com" });
+
+    expect(error).toEqual(left(new InvalidNameError()));
+  })
 
 });
