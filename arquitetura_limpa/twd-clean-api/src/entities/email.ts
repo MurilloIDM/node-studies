55acf1105d30b7c0d16d5b453986b1d8ac6720a0
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-15 20:05:55 -0300

    feat: verify e-mail address when creating a user

--- arquitetura_limpa/twd-clean-api/src/entities/email.ts
+++ arquitetura_limpa/twd-clean-api/src/entities/email.ts
@@ -1,19 +1,36 @@
+import { Either, left, right } from "../shared/either";
+import { InvalidEmailError } from "./errors/invalid-email-error";
+
 export class Email {
+  private readonly email: string;
+
+  constructor(email: string) {
+    this.email = email;
+  }
+
+  static create(email: string): Either<InvalidEmailError, Email> {
+    if (Email.validate(email)) {
+      return right(new Email(email));
+    }
+
+    return left(new InvalidEmailError());
+  }
+
   static validate(email: string): boolean {
     if (!email) return false;
 
-    if (email.length > 320) return false;
+    if (email?.length > 320) return false;
 
-    const [local, domain] = email.split("@");
+    const [local, domain] = email?.split("@");
 
-    const lengthLocal = local.length;
-    const lengthDomain = domain.length;
+    const lengthLocal = local?.length;
+    const lengthDomain = domain?.length;
 
     if (lengthLocal === 0 || lengthLocal > 64 || lengthDomain === 0 || lengthDomain > 255) return false;
 
-    const domainParts = domain.split(".");
+    const domainParts = domain?.split(".");
 
-    if (domainParts.some((part) => part.length > 63)) return false;
+    if (domainParts?.some((part) => part?.length > 63)) return false;
 
     const emailRegex =
     /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
