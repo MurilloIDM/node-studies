ee6f614abc57bff701ce0b5c4bb0e95dbd579b86
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-22 20:07:35 -0300

    test: don't user with invalid email

--- arquitetura_limpa/twd-clean-api/src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ arquitetura_limpa/twd-clean-api/src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,7 @@
+import { InvalidEmailError } from "../../entities/errors/invalid-email-error";
+import { InvalidNameError } from "../../entities/errors/invalid-name-error";
 import { UserData } from "../../entities/user-data";
+import { left } from "../../shared/either";
 import { UserRepository } from "./ports/user-repository";
 import { RegisterUserOnMailingList } from "./register-user-on-mailing-list";
 import { InMemoryUserRepository } from "./repository/in-memory-user-repository";
@@ -19,6 +22,21 @@ describe("Register user on mailing list use case", () => {
 
     expect(user.name).toBe("any_name");
     expect(response.value.name).toBe("any_name");
+  });
+
+  test("should not add user with invalid email to mailing list", async () => {
+    const users: UserData[] = [];
+    const repo: UserRepository = new InMemoryUserRepository(users);
+    const usercase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
+
+    const name = "any_name";
+    const invalidEmail = "invalid_email";
+
+    const response = await usercase.execute({ name, email: invalidEmail });
+    const user = await repo.findUserByEmail(invalidEmail);
+
+    expect(user).toBeNull();
+    expect(response).toEqual(left(new InvalidEmailError()));    
   });
 
 });
