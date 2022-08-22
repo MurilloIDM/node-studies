6bb65f6a5a59f76e2355105411cda5d0cdf434b4
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-22 19:59:23 -0300

    test: register valid user to mailing list

--- arquitetura_limpa/twd-clean-api/src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ arquitetura_limpa/twd-clean-api/src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,20 +1,24 @@
 import { UserData } from "../../entities/user-data";
+import { UserRepository } from "./ports/user-repository";
+import { RegisterUserOnMailingList } from "./register-user-on-mailing-list";
+import { InMemoryUserRepository } from "./repository/in-memory-user-repository";
 
 describe("Register user on mailing list use case", () => {
 
   test("should add user with complete data to mailing list", async () => {
-    // const users: UserData[] = [];
-    // const repo: UserRepository = new InMemoryUserRepository(users);
-    // const usercase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
+    const users: UserData[] = [];
+    const repo: UserRepository = new InMemoryUserRepository(users);
+    const usercase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
 
-    // const name = "any_name";
-    // const email = "any@email.com";
+    const name = "any_name";
+    const email = "any@email.com";
 
-    // const response = await usercase.registerUserOnMailingList({ name, email });
+    const response = await usercase.execute({ name, email });
 
-    // const user = await repo.findUserByEmail("any@email.com");
+    const user = await repo.findUserByEmail("any@email.com");
 
-    // expect(user.name).toBe("any_name");
+    expect(user.name).toBe("any_name");
+    expect(response.value.name).toBe("any_name");
   });
 
 });
