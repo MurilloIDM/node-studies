6216ec12d7dec382e4367e0e688357083bb6e2fe
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-24 20:51:02 -0300

    feat: web controller should request is missing parameters

--- arquitetura_limpa/twd-clean-api/src/web-controllers/register-user-controller.ts
+++ arquitetura_limpa/twd-clean-api/src/web-controllers/register-user-controller.ts
@@ -1,7 +1,8 @@
 import { UserData } from "@/entities";
 import { RegisterUserOnMailingList } from "@/useCases/register-user-on-mailing-list";
+import { MissingParamError } from "@/web-controllers/errors";
 import { HttpRequest, HttpResponse } from "@/web-controllers/ports";
-import { created } from "@/web-controllers/utils";
+import { badRequest, created } from "@/web-controllers/utils";
 
 export class RegisterUserController {
   private readonly usecase: RegisterUserOnMailingList;
@@ -11,6 +12,14 @@ export class RegisterUserController {
   }
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
+    if (!request.body.name || !request.body.email) {
+      const missingParam =
+        !request.body.name && !request.body.email ? "name and email" :
+        !request.body.name ? "name" : 
+        !request.body.email ? "email" : "";
+      return badRequest(new MissingParamError(missingParam));
+    }
+
     const userData: UserData = request.body;
 
     const response = await this.usecase.execute(userData);
@@ -18,5 +27,7 @@ export class RegisterUserController {
     if (response.isRight()) {
       return created(response.value);
     }
+
+    return badRequest(response.value);
   }
 }
