e65548ffb6c98f9a3af851c15c936a504590b084
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-26 22:05:56 -0300

    feat: apply content type middleware

--- arquitetura_limpa/twd-clean-api/src/main/config/middleware/index.ts
+++ arquitetura_limpa/twd-clean-api/src/main/config/middleware/index.ts
@@ -1,3 +1,4 @@
 export * from "./body-parser";
 export * from "./middleware";
 export * from "./cors";
+export * from "./content-type";
--- arquitetura_limpa/twd-clean-api/src/main/config/middleware/middleware.ts
+++ arquitetura_limpa/twd-clean-api/src/main/config/middleware/middleware.ts
@@ -1,8 +1,8 @@
-import { bodyParser } from "@/main/config/middleware/body-parser";
-import { cors } from "@/main/config/middleware/cors";
+import { bodyParser, contentType, cors } from "@/main/config/middleware";
 import { Express } from "express";
 
 export const setupMiddleware = (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 }
